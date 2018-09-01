af42b027219188a4069fe2d491f980514a9b4d10
Shane Lester <Shanejlester@gmail.com>
2018-09-01 14:53:26 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// function toggleFooter(event) {
+// 	if (document.body.scrollTop < 10) {
+// 		document.querySelector('footer').style = 'display: block';
+// 		// alert('here');
+// 	} else {
+// 		document.querySelector('footer').style = 'display: hidden';
+// 	}
+// }
+
+// window.addEventListener('scroll', toggleFooter);
