29447a3bfb04c7c62a6e911caaa215e4eae75f98
Shane Lester <Shanejlester@gmail.com>
2018-09-01 20:36:46 -0400

    Add logo accets

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-// function toggleFooter(event) {
-// 	if (document.body.scrollTop < 10) {
-// 		document.querySelector('footer').style = 'display: block';
-// 		// alert('here');
-// 	} else {
-// 		document.querySelector('footer').style = 'display: hidden';
-// 	}
-// }
+class AppInformation {
+	constructor(appName) {
+		const info = AppInformation.generateAppInfo(appName);
+		this.svgImage = info[0];
+		this.nameOfGame = info[1];
+		this.gameDescription = info[2];
+	}
 
-// window.addEventListener('scroll', toggleFooter);
+	static generateAppInfo(appName) {
+		let info = [];
+		if (appName === 'Tommy Cross') {
+			info.push('images/tommy.svg');
+			info.push('Tommy Cross');
+			const gameDescription = 'Welcome to Tommy Cross :)';
+			info.push(gameDescription);
+		}
+		else if (appName === 'Concentration') {
+			info.push('images/concentration.svg');
+			info.push('Concentration');
+			const gameDescription = 'Welcome to Concentration :)';
+			info.push(gameDescription);
+		}
+	}
+}
+
+class ActiveGame {
+	constructor() {
+		this.apps = [new AppInformation('Concentration'), new AppInformation('Tommy Cross')];
+		this.currentAppIndex = 0;
+	}
+	//moveLeft()
+	//moveRight()
+}
+
+
+
+document.querySelector('')
